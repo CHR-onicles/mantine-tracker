3846972d263120b5aa81032e7639c86097f4a95b
Divine Anum <tpandivine48@gmail.com>
2024-02-20 19:47:25 +0000

    Update index.tsx

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Stack, Title } from "@mantine/core";
+import { Container, Stack, Title } from "@mantine/core";
 
 import { AppShellLayout } from "@layouts/AppShell";
 
@@ -12,18 +12,20 @@ export const Dashboard = () => {
   const [date, setDate] = useState<Date | null>(new Date());
   return (
     <AppShellLayout>
-      <Stack gap={"3rem"}>
-        <OverviewCards date={date} setDate={setDate} />
+      <Container maw={1360}>
+        <Stack gap={"3rem"} mt={"xl"}>
+          <OverviewCards date={date} setDate={setDate} />
 
-        <Stack mt={"sm"}>
-          <Title order={3} fw={600} fz={20} mb={"xs"} c={"dimmed"}>
-            Yearly activity for {date?.getFullYear()}
-          </Title>
-          <Charts />
+          <Stack mt={"sm"}>
+            <Title order={3} fw={600} fz={20} mb={"xs"} c={"dimmed"}>
+              Yearly activity for {date?.getFullYear()}
+            </Title>
+            <Charts />
+          </Stack>
+
+          <RecentActivityTable />
         </Stack>
-
-        <RecentActivityTable />
-      </Stack>
+      </Container>
     </AppShellLayout>
   );
 };
